1d114964d7b0d93656d1118d7de478c38f6aa152
Macielsbdev <macielbezerra2018@gmail.com>
2025-04-24 16:58:07 -0300

    noeme da banda

--- index.js
+++ index.js
@@ -1,93 +1,11 @@
-//Questão 1
-// let idadeMinima = 17;
+let mes = prompt("Digite o mês do seu nascimento(ex: jan):");
+let dia = prompt("Digite o dia do seu nascimento(ex:1-31):");
+let nomeDaBanda = "";
 
-// if(idadeMinima >= 18){
-//     console.log("Maior de idade");
-// }else{
-//     console.log("Menor de idade");
-// }
-
-//Questão 2
-
-// let numero = 10;
-
-// if(numero > 0){
-//     console.log("Positivo");
-// } else if(numero == 0){
-//     console.log("Zero");
-// } else {
-//     console.log("Negativo");
-// }
-
-//Questão 3
-
-// let numeroA = 3;
-// let numeroB = 3;
-
-// if(numeroA > numeroB){
-//     console.log(`${numeroA} é maior que ${numeroB}`);
-// } else if(numeroB > numeroA){
-//     console.log(`${numeroB} é maior que ${numeroA}`);
-// } else {
-//     console.log("Os números são iguais");
-// }
-
-//Questão 4
-
-// let numero = 44;
-
-// if((numero % 2) == 0){
-//     console.log(`${numero} é par!`);
-// }else{
-//     console.log(`${numero} é impar!`);
-// }
-
-// Questão 5 
-
-// let notaA = 7;
-// let notaB = 7;
-// let notaC = 8;
-// let media = ((notaA + notaB + notaC) / 3).toFixed(1);
-// let situacao = "";
-
-// if (media >= 7) {
-//     situacao = "Aprovado";
-// } else if (media > 5 && media < 7) {
-//     situacao = "Recuperação";
-// } else {
-//     situacao = "Reprovado";
-// }
-
-// console.log(`${situacao}, média: ${media}`);
-
-//Questão 6
-
-// let valor = 620;
-// let desconto = 0.35;
-// if (valor > 100) {
-//     console.log(`Total a pagar: R$ ${(valor - (valor * desconto)).toFixed(2)}`);
-// } else {
-//     console.log(`Total a pagar: R$ ${valor.toFixed(2)}`);
-// }
-
-//Questão 7
-
-let ano = 2023;
-
-if (ano % 400 == 0) {
-    console.log(`${ano} é bisexto`);
-} else if (ano % 4 == 0 && ano % 100 != 0) {
-    console.log(`${ano} é bisexto`);
-} else {
-    console.log(`${ano} não é bisexto`);
+switch(mes){
+    case "jan":
+        nomeDaBanda = "Capirotos";
+        break;
 }
 
-let acesso = usuario && senha
-
-if (acesso == admin && 1234) {
-    console.log(`${acesso} permitido`);
-}
-
-if (acesso != admim && 1234) {
-    console.log(`&{acesso} negado`)
-}
+alert(`O nome da sua banda é ${nomeDaBanda}`)
