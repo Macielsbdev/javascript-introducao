422956ef859961a9b71499a12979bb073cbfafa3
Macielsbdev <macielbezerra2018@gmail.com>
2025-04-25 10:46:39 -0300

    exercicio

--- index.js
+++ index.js
@@ -6,6 +6,39 @@ switch(mes){
     case "jan":
         nomeDaBanda = "Capirotos";
         break;
+    case "fev":
+        nomeDaBanda = "Lendários";
+        break;
+    case "mar":
+        nomeDaBanda = "Soldados";
+        break;        
+    case "abr":
+        nomeDaBanda = "Faroestes";
+        break;
+    case "mai":
+        nomeDaBanda = "Fodões";
+        break;
+    case "jun":
+        nomeDaBanda = "Monstros";
+        break;
+    case "jul":
+        nomeDaBanda = "Fanfarrões";
+        break;
+    case "ago":
+        nomeDaBanda = "Baderneiros";
+        break;
+    case "set":
+        nomeDaBanda = "Perturbados";
+        break;
+    case "out":
+        nomeDaBanda = "Aliens";
+        break;
+    case "nov":
+        nomeDaBanda = "Revoltados";
+        break;
+    case "dez":
+        nomeDaBanda = "Gigantes";
+        break;
 }
 
 alert(`O nome da sua banda é ${nomeDaBanda}`)
